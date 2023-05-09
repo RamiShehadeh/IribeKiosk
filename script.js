7dfca476edc770633fb7e690aef09a3579f6e4eb
EdmundLi <edmund.li427@gmail.com>
2023-05-08 20:23:15 -0400

    keybaord

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Load the content of each tab from separate HTML files
     $("#alerts").load("tabs/tab0.html");
-    $("#building-map").load("tabs/tab1.html");
+    // $("#building-map").load("tabs/tab1.html");
     $("#campus-map").load("tabs/tab2.html");
-    $("#hours").load("tabs/tab3.html");
+    // $("#hours").load("tabs/tab3.html");
     $("#about").load("tabs/tab5.html");
     $("#alertz").load("tabs/tab6.html");
     
@@ -316,5 +316,235 @@ $(".alerts-container").load("tab0.html .alert, .notification", function() {
 
   scrollAlerts();
 });
+
+
+
+
+
+
+//eddy stuff
+
+
+
+
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+  function favTutorial() {
+    var mylist = document.getElementById("myList");
+    document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
+  }
+
+  function openForm() {
+document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+document.getElementById("myForm").style.display = "none";
+}
+
+/*keeb*/
+const Keyboard = {
+  elements: {
+  main: null,
+  keysContainer: null,
+  keys: []
+},
+
+eventHandlers: {
+  oninput: null,
+  onclose: null
+},
+
+properties: {
+  value: "",
+  capsLock: false
+},
+
+
+init() {
+  // Create main elements
+  this.elements.main = document.createElement("div");
+  this.elements.keysContainer = document.createElement("div");
+
+  // Setup main elements
+  this.elements.main.classList.add("keyboard", "keyboard--hidden");
+  this.elements.keysContainer.classList.add("keyboard__keys");
+  this.elements.keysContainer.appendChild(this._createKeys());
+
+  this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+
+  // Add to DOM
+  this.elements.main.appendChild(this.elements.keysContainer);
+  document.body.appendChild(this.elements.main);
+
+  // Automatically use keyboard for elements with .use-keyboard-input
+  document.querySelectorAll(".use-keyboard-input").forEach(element => {
+      element.addEventListener("focus", () => {
+          this.open(element.value, currentValue => {
+              element.value = currentValue;
+          });
+      });
+  });
+},
+
+
+_createKeys() {
+  const fragment = document.createDocumentFragment();
+  const keyLayout = [
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+  "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+  "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+  "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+  "space"
+];
+
+// Creates HTML for an icon
+const createIconHTML = (icon_name) => {
+return `<i class="material-icons">${icon_name}</i>`;
+};
+
+keyLayout.forEach(key => {
+const keyElement = document.createElement("button");
+const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+// Add attributes/classes
+keyElement.setAttribute("type", "button");
+keyElement.classList.add("keyboard__key");
+
+switch (key) {
+case "backspace":
+keyElement.classList.add("keyboard__key--wide");
+keyElement.innerHTML = createIconHTML("backspace");
+
+keyElement.addEventListener("click", () => {
+    this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+    this._triggerEvent("oninput");
+});
+
+break;
+
+case "caps":
+keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+keyElement.addEventListener("click", () => {
+    this._toggleCapsLock();
+    keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+});
+
+break;
+
+case "enter":
+keyElement.classList.add("keyboard__key--wide");
+keyElement.innerHTML = createIconHTML("keyboard_return");
+
+keyElement.addEventListener("click", () => {
+    this.properties.value += "\n";
+    this._triggerEvent("oninput");
+});
+
+break;
+
+case "space":
+keyElement.classList.add("keyboard__key--extra-wide");
+keyElement.innerHTML = createIconHTML("space_bar");
+
+keyElement.addEventListener("click", () => {
+    this.properties.value += " ";
+    this._triggerEvent("oninput");
+});
+
+break;
+
+case "done":
+keyElement.classList.add("keyboard__key--wide", "keyboard__key--dark");
+keyElement.innerHTML = createIconHTML("check_circle");
+
+keyElement.addEventListener("click", () => {
+    this.close();
+    this._triggerEvent("onclose");
+});
+
+break;
+
+default:
+keyElement.textContent = key.toLowerCase();
+
+keyElement.addEventListener("click", () => {
+    this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+    this._triggerEvent("oninput");
+});
+
+break;
+}
+
+fragment.appendChild(keyElement);
+
+if (insertLineBreak) {
+fragment.appendChild(document.createElement("br"));
+}
+});
+
+return fragment;
+},
+
+_triggerEvent(handlerName) {
+if (typeof this.eventHandlers[handlerName] == "function") {
+this.eventHandlers[handlerName](this.properties.value);
+}
+},
+
+_toggleCapsLock() {
+this.properties.capsLock = !this.properties.capsLock;
+
+for (const key of this.elements.keys) {
+if (key.childElementCount === 0) {
+key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+}
+}
+},
+
+open(initialValue, oninput, onclose) {
+this.properties.value = initialValue || "";
+this.eventHandlers.oninput = oninput;
+this.eventHandlers.onclose = onclose;
+this.elements.main.classList.remove("keyboard--hidden");
+},
+
+close() {
+this.properties.value = "";
+this.eventHandlers.oninput = oninput;
+this.eventHandlers.onclose = onclose;
+this.elements.main.classList.add("keyboard--hidden");
+}
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+Keyboard.init();
+});
+
+
+
+
 
 
